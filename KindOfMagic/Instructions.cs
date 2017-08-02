611928b9865855443746758979b026d9648b5101
Éli Marshal <elimarshal@gmail.com>
2017-08-02 07:00:36 -0600

    Update conventions for Cecil 10

--- KindOfMagic/Instructions.cs
+++ KindOfMagic/Instructions.cs
@@ -1,165 +1,165 @@
-﻿using System.Collections.Generic;
-using Mono.Cecil;
+﻿using Mono.Cecil;
 using Mono.Cecil.Cil;
 using Mono.Collections.Generic;
+using System.Collections.Generic;
 
 namespace KindOfMagic
 {
-  class Instructions : List<Instruction>
-  {
-    public void Add(OpCode opCode)
+    internal class Instructions : List<Instruction>
     {
-      Add(Instruction.Create(opCode));
-    }
-
-    public void InsertInto(Collection<Instruction> target, int position = 0)
-    {
-      foreach (var i in this)
-      {
-        target.Insert(position, i);
-        position++;
-      }
-    }
-
-    public void MergeInto(Collection<Instruction> target, int position = 0)
-    {
-      var first = true;
-
-      foreach (var i in this)
-      {
-        if (first)
+        public void Add(OpCode opCode)
         {
-          first = false;
-          var op = target[position];
-          op.Operand = i.Operand;
-          op.OpCode = i.OpCode;
+            Add(Instruction.Create(opCode));
         }
-        else
+
+        public void InsertInto(Collection<Instruction> target, int position = 0)
         {
-          var op = Instruction.Create(OpCodes.Nop);
-          op.Operand = i.Operand;
-          op.OpCode = i.OpCode;
-          target.Insert(position, op);
+            foreach (var i in this)
+            {
+                target.Insert(position, i);
+                position++;
+            }
         }
-        position++;
-      }
-    }
 
-    public void Call(MethodReference method)
-    {
-      Add(Instruction.Create(OpCodes.Call, method));
-    }
+        public void MergeInto(Collection<Instruction> target, int position = 0)
+        {
+            var first = true;
 
-    internal void Callvirt(MethodReference method)
-    {
-      Add(Instruction.Create(OpCodes.Callvirt, method));
-    }
+            foreach (var i in this)
+            {
+                if (first)
+                {
+                    first = false;
+                    var op = target[position];
+                    op.Operand = i.Operand;
+                    op.OpCode = i.OpCode;
+                }
+                else
+                {
+                    var op = Instruction.Create(OpCodes.Nop);
+                    op.Operand = i.Operand;
+                    op.OpCode = i.OpCode;
+                    target.Insert(position, op);
+                }
+                position++;
+            }
+        }
 
-    public void Ldarg_0()
-    {
-      Add(Instruction.Create(OpCodes.Ldarg_0));
-    }
+        public void Call(MethodReference method)
+        {
+            Add(Instruction.Create(OpCodes.Call, method));
+        }
 
-    public void Ldarg_1()
-    {
-      Add(Instruction.Create(OpCodes.Ldarg_1));
-    }
+        internal void Callvirt(MethodReference method)
+        {
+            Add(Instruction.Create(OpCodes.Callvirt, method));
+        }
 
-    public void Ldfld(FieldReference field)
-    {
-      Add(Instruction.Create(OpCodes.Ldfld, field));
-    }
+        public void Ldarg_0()
+        {
+            Add(Instruction.Create(OpCodes.Ldarg_0));
+        }
 
-    public void Stloc_S(VariableDefinition var)
-    {
-      Add(Instruction.Create(OpCodes.Stloc_S, var));
-    }
+        public void Ldarg_1()
+        {
+            Add(Instruction.Create(OpCodes.Ldarg_1));
+        }
 
-    public Instruction Ldloc(VariableDefinition var)
-    {
-      var result = Instruction.Create(OpCodes.Ldloc, var);
-      Add(result);
-      return result;
-    }
+        public void Ldfld(FieldReference field)
+        {
+            Add(Instruction.Create(OpCodes.Ldfld, field));
+        }
 
-    public void Bne_Un_S(Instruction target)
-    {
-      Add(Instruction.Create(OpCodes.Bne_Un_S, target));
-    }
+        public void Stloc_S(VariableDefinition var)
+        {
+            Add(Instruction.Create(OpCodes.Stloc_S, var));
+        }
 
-    public void Bne_Un(Instruction target)
-    {
-      Add(Instruction.Create(OpCodes.Bne_Un, target));
-    }
+        public Instruction Ldloc(VariableDefinition var)
+        {
+            var result = Instruction.Create(OpCodes.Ldloc, var);
+            Add(result);
+            return result;
+        }
 
-    public void Ceq()
-    {
-      Add(Instruction.Create(OpCodes.Ceq));
-    }
+        public void Bne_Un_S(Instruction target)
+        {
+            Add(Instruction.Create(OpCodes.Bne_Un_S, target));
+        }
 
-    public void Brtrue_S(Instruction target)
-    {
-      Add(Instruction.Create(OpCodes.Brtrue_S, target));
-    }
+        public void Bne_Un(Instruction target)
+        {
+            Add(Instruction.Create(OpCodes.Bne_Un, target));
+        }
 
-    public void Brfalse_S(Instruction target)
-    {
-      Add(Instruction.Create(OpCodes.Brfalse_S, target));
-    }
+        public void Ceq()
+        {
+            Add(Instruction.Create(OpCodes.Ceq));
+        }
 
-    public void Ldstr(string str)
-    {
-      Add(Instruction.Create(OpCodes.Ldstr, str));
-    }
+        public void Brtrue_S(Instruction target)
+        {
+            Add(Instruction.Create(OpCodes.Brtrue_S, target));
+        }
 
-    public void Beq_S(Instruction target)
-    {
-      Add(Instruction.Create(OpCodes.Beq_S, target));
-    }
+        public void Brfalse_S(Instruction target)
+        {
+            Add(Instruction.Create(OpCodes.Brfalse_S, target));
+        }
 
-    public void Beq(Instruction target)
-    {
-      Add(Instruction.Create(OpCodes.Beq, target));
-    }
+        public void Ldstr(string str)
+        {
+            Add(Instruction.Create(OpCodes.Ldstr, str));
+        }
 
-    public void CastClass(TypeReference type)
-    {
-      Add(Instruction.Create(OpCodes.Castclass, type));
-    }
+        public void Beq_S(Instruction target)
+        {
+            Add(Instruction.Create(OpCodes.Beq_S, target));
+        }
 
-    public void Ldflda(FieldReference field)
-    {
-      Add(Instruction.Create(OpCodes.Ldflda, field));
-    }
+        public void Beq(Instruction target)
+        {
+            Add(Instruction.Create(OpCodes.Beq, target));
+        }
 
-    public void Ret()
-    {
-      Add(Instruction.Create(OpCodes.Ret));
-    }
+        public void CastClass(TypeReference type)
+        {
+            Add(Instruction.Create(OpCodes.Castclass, type));
+        }
 
-    public void Ldc_i4_0()
-    {
-      Add(Instruction.Create(OpCodes.Ldc_I4_0));
-    }
+        public void Ldflda(FieldReference field)
+        {
+            Add(Instruction.Create(OpCodes.Ldflda, field));
+        }
 
-    public void Ldloca_S(VariableDefinition var)
-    {
-      Add(Instruction.Create(OpCodes.Ldloca_S, var));
-    }
+        public void Ret()
+        {
+            Add(Instruction.Create(OpCodes.Ret));
+        }
 
-    internal void Ldarga_S(ParameterDefinition param)
-    {
-      Add(Instruction.Create(OpCodes.Ldarga_S, param));
-    }
+        public void Ldc_i4_0()
+        {
+            Add(Instruction.Create(OpCodes.Ldc_I4_0));
+        }
 
-    internal void Nop()
-    {
-      Add(Instruction.Create(OpCodes.Nop));
-    }
+        public void Ldloca_S(VariableDefinition var)
+        {
+            Add(Instruction.Create(OpCodes.Ldloca_S, var));
+        }
 
-    internal void Stloc_0()
-    {
-      Add(Instruction.Create(OpCodes.Stloc_0));
+        internal void Ldarga_S(ParameterDefinition param)
+        {
+            Add(Instruction.Create(OpCodes.Ldarga_S, param));
+        }
+
+        internal void Nop()
+        {
+            Add(Instruction.Create(OpCodes.Nop));
+        }
+
+        internal void Stloc_0()
+        {
+            Add(Instruction.Create(OpCodes.Stloc_0));
+        }
     }
-  }
 }
--- KindOfMagic/MagicTask.cs
+++ KindOfMagic/MagicTask.cs
@@ -1,264 +1,280 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using Microsoft.Build.Framework;
+﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using Mono.Cecil;
 using Mono.Cecil.Pdb;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace KindOfMagic
 {
-  public class MagicTask : Task
-  {
-    [Required]
-    public string Assembly { get; set; }
-
-    [Required]
-    public string References { get; set; }
-    public string KeyFile { get; set; }
-
-    public string RaiseMethod { get; set; }
-    public string BeaconMethod { get; set; }
-    public string MagicAttribute { get; set; }
-    public string NoMagicAttribute { get; set; }
-
-    class PreloadingAssemblyResolver : DefaultAssemblyResolver
+    public class MagicTask : Task
     {
-      public PreloadingAssemblyResolver(string references)
-      {
-        Load(references);
-      }
+        [Required]
+        public string Assembly { get; set; }
 
-      public bool NoGAC;
+        [Required]
+        public string References { get; set; }
+        public string KeyFile { get; set; }
 
-      AssemblyDefinition _systemRuntime;
+        public string RaiseMethod { get; set; }
+        public string BeaconMethod { get; set; }
+        public string MagicAttribute { get; set; }
+        public string NoMagicAttribute { get; set; }
 
-      public override AssemblyDefinition Resolve(AssemblyNameReference name)
-      {
-        if (name.Name == "System.Runtime")
-          return ResolveSystemRuntime(name);
-
-        return base.Resolve(name);
-      }
-
-      AssemblyDefinition ResolveSystemRuntime(AssemblyNameReference name)
-      {
-        return _systemRuntime ?? (_systemRuntime = base.Resolve(name));
-      }
-
-      public override AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
-      {
-        return NoGAC ? ResolveNoGAC(name, ref parameters) : base.Resolve(name, parameters);
-      }
-
-      public readonly List<string> Directories = new List<string> { "." };
-
-      public void AddSilverlightDirectories(int version)
-      {
-        var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
-        Directories.Add(Path.Combine(programFiles, string.Format(@"Reference Assemblies\Microsoft\Framework\Silverlight\v{0}.0", version)));
-        Directories.Add(Path.Combine(programFiles, string.Format(@"Microsoft SDKs\Silverlight\v{0}.0\Libraries\Client", version)));
-        NoGAC = true;
-      }
-
-      AssemblyDefinition ResolveNoGAC(AssemblyNameReference name, ref ReaderParameters parameters)
-      {
-        if (name == null)
-          throw new ArgumentNullException("name");
-
-        if (parameters == null)
-          parameters = new ReaderParameters();
-
-        var assembly = SearchDirectory(name, Directories, parameters);
-        if (assembly != null)
-          return assembly;
-
-        throw new AssemblyResolutionException(name);
-      }
-
-      AssemblyDefinition GetAssembly(string file, ReaderParameters parameters)
-      {
-        if (parameters.AssemblyResolver == null)
-          parameters.AssemblyResolver = this;
-
-        return ModuleDefinition.ReadModule(file, parameters).Assembly;
-      }
-
-      AssemblyDefinition SearchDirectory(AssemblyNameReference name, IEnumerable<string> directories, ReaderParameters parameters)
-      {
-        var extensions = new[] { ".dll", ".exe" };
-        foreach (var directory in directories)
+        private class PreloadingAssemblyResolver : DefaultAssemblyResolver
         {
-          foreach (var extension in extensions)
-          {
-            string file = Path.Combine(directory, name.Name + extension);
-            if (File.Exists(file))
-              return GetAssembly(file, parameters);
-          }
+            public PreloadingAssemblyResolver(string references)
+            {
+                ////Load(references);
+            }
+
+            public bool NoGAC;
+
+            private AssemblyDefinition _systemRuntime;
+
+            public override AssemblyDefinition Resolve(AssemblyNameReference name)
+            {
+                if (name.Name == "System.Runtime")
+                    return ResolveSystemRuntime(name);
+
+                return base.Resolve(name);
+            }
+
+            private AssemblyDefinition ResolveSystemRuntime(AssemblyNameReference name)
+            {
+                return _systemRuntime ?? (_systemRuntime = base.Resolve(name));
+            }
+
+            public override AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+            {
+                return NoGAC ? ResolveNoGAC(name, ref parameters) : base.Resolve(name, parameters);
+            }
+
+            public readonly List<string> Directories = new List<string> { "." };
+
+            public void AddSilverlightDirectories(int version)
+            {
+                var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+                Directories.Add(Path.Combine(programFiles, string.Format(@"Reference Assemblies\Microsoft\Framework\Silverlight\v{0}.0", version)));
+                Directories.Add(Path.Combine(programFiles, string.Format(@"Microsoft SDKs\Silverlight\v{0}.0\Libraries\Client", version)));
+                NoGAC = true;
+            }
+
+            private AssemblyDefinition ResolveNoGAC(AssemblyNameReference name, ref ReaderParameters parameters)
+            {
+                if (name == null)
+                    throw new ArgumentNullException("name");
+
+                if (parameters == null)
+                    parameters = new ReaderParameters() { ReadWrite = true };
+
+                var assembly = SearchDirectory(name, Directories, parameters);
+                if (assembly != null)
+                    return assembly;
+
+                throw new AssemblyResolutionException(name);
+            }
+
+            private AssemblyDefinition GetAssembly(string file, ReaderParameters parameters)
+            {
+                if (parameters.AssemblyResolver == null)
+                    parameters.AssemblyResolver = this;
+
+                return ModuleDefinition.ReadModule(file, parameters).Assembly;
+            }
+
+            private AssemblyDefinition SearchDirectory(AssemblyNameReference name, IEnumerable<string> directories, ReaderParameters parameters)
+            {
+                var extensions = new[] { ".dll", ".exe" };
+                foreach (var directory in directories)
+                {
+                    foreach (var extension in extensions)
+                    {
+                        string file = Path.Combine(directory, name.Name + extension);
+                        if (File.Exists(file))
+                            return GetAssembly(file, parameters);
+                    }
+                }
+
+                return null;
+            }
+
+            public void Load(params string[] references)
+            {
+                foreach (var dll in references)
+                {
+                    try
+                    {
+                        using (var assembly = ModuleDefinition.ReadModule(dll, new ReaderParameters { AssemblyResolver = this, ReadWrite = true }).Assembly)
+                        {
+                            if (assembly.Name.Name == "System.Runtime")
+                                _systemRuntime = assembly;
+
+                            RegisterAssembly(assembly);
+                        }
+                    }
+                    catch (UnauthorizedAccessException) { }
+                }
+            }
+
+            public void Load(string references)
+            {
+                if (references != null)
+                    Load(references.Split(';'));
+            }
         }
 
-        return null;
-      }
-
-      public void Load(params string[] references)
-      {
-        foreach (var dll in references)
+        private void TaskLogger(LogLevel level, string format, params object[] args)
         {
-          var assembly = ModuleDefinition.ReadModule(dll, new ReaderParameters { AssemblyResolver = this }).Assembly;
-          
-          if (assembly.Name.Name == "System.Runtime")
-            _systemRuntime = assembly;
-
-          RegisterAssembly(assembly);
-        }
-      }
-
-      public void Load(string references)
-      {
-        if (references != null)
-          Load(references.Split(';'));
-      }
-    }
-
-    void TaskLogger(LogLevel level, string format, params object[] args)
-    {
-      switch (level)
-      {
-        case LogLevel.Error: Log.LogError("Build", "", "", "", -1, -1, -1, -1, format, args); break;
-        case LogLevel.Message: Log.LogMessage("Build", "", "", "", -1, -1, -1, -1, format, args); break;
-        case LogLevel.Warning: Log.LogWarning("Build", "", "", "", -1, -1, -1, -1, format, args); break;
-        case LogLevel.Verbose: Log.LogMessage(format, args); break; // goes into MSBuild log 
-      }
-    }
-
-    public override bool Execute()
-    {
-      Log.LogMessage("Magic happens with {0}.", Assembly);
-      try
-      {
-        var resolver = new PreloadingAssemblyResolver(References);
-
-        var parameters = MakeReaderParameters(Assembly, resolver);
-        var assembly = AssemblyDefinition.ReadAssembly(Assembly, parameters);
-
-        var processor = new Processor(parameters.AssemblyResolver, assembly.MainModule)
-        {
-          MagicAttributeName = MagicAttribute,
-          NoMagicAttributeName = NoMagicAttribute,
-          RaiseMethodName = RaiseMethod,
-          BeaconMethodName = BeaconMethod,
-          Logger = TaskLogger
-        };
-
-        if (!processor.NeedProcessing)
-        {
-          Log.LogMessage("Assembly is already enchanted.");
-          return true;
+            switch (level)
+            {
+                case LogLevel.Error: Log.LogError("Build", "", "", "", -1, -1, -1, -1, format, args); break;
+                case LogLevel.Message: Log.LogMessage("Build", "", "", "", -1, -1, -1, -1, format, args); break;
+                case LogLevel.Warning: Log.LogWarning("Build", "", "", "", -1, -1, -1, -1, format, args); break;
+                case LogLevel.Verbose: Log.LogMessage(format, args); break; // goes into MSBuild log
+            }
         }
 
-        Log.LogMessage("Enchanting assembly...");
-        var result = processor.Process();
-
-        switch (result)
+        public override bool Execute()
         {
-          case MagicResult.NoMagicFound: Log.LogMessage("No Magic attributes found..."); return true;
-          case MagicResult.NoMagicNeeded: Log.LogMessage("Enchanting not needed..."); break;
-          case MagicResult.MagicApplied: Log.LogMessage("Enchanted successfully..."); break;
+            Log.LogMessage("Magic happens with {0}.", Assembly);
+            try
+            {
+                Log.LogMessage("Enchanting assembly...#6");
+                using (var resolver = new PreloadingAssemblyResolver(References))
+                {
+                    Log.LogMessage("Enchanting assembly...#3");
+                    var parameters = MakeReaderParameters(Assembly, resolver);
+                    Log.LogMessage("Enchanting assembly...#4");
+                    using (var assembly = AssemblyDefinition.ReadAssembly(Assembly, parameters))
+                    {
+                        Log.LogMessage("Enchanting assembly...#5");
+                        using (var processor = new Processor(parameters.AssemblyResolver, assembly.MainModule)
+                        {
+                            MagicAttributeName = MagicAttribute,
+                            NoMagicAttributeName = NoMagicAttribute,
+                            RaiseMethodName = RaiseMethod,
+                            BeaconMethodName = BeaconMethod,
+                            Logger = TaskLogger
+                        })
+                        {
+                            if (!processor.NeedProcessing)
+                            {
+                                Log.LogMessage("Assembly is already enchanted.");
+                                return true;
+                            }
+
+                            Log.LogMessage("Enchanting assembly...");
+                            var result = processor.Process();
+
+                            switch (result)
+                            {
+                                case MagicResult.NoMagicFound: Log.LogMessage("No Magic attributes found..."); return true;
+                                case MagicResult.NoMagicNeeded: Log.LogMessage("Enchanting not needed..."); break;
+                                case MagicResult.MagicApplied: Log.LogMessage("Enchanted successfully..."); break;
+                            }
+                        }
+
+                        if (KeyFile != null)
+                            Log.LogMessage("Resigning enchanted assembly with key from {0}...", KeyFile);
+
+                        assembly.Write(Assembly, MakeWriterParameters(Assembly, KeyFile));
+                        Log.LogMessage("Enchanted successfully...#2");
+                    }
+                }
+                return true;
+            }
+            catch (FileNotFoundException)
+            {
+                // no files - no fun
+                return false;
+            }
+            catch (Exception e)
+            {
+                Log.LogMessage(e.Message);
+                return false;
+            }
         }
 
-        if (KeyFile != null)
-          Log.LogMessage("Resigning enchanted assembly with key from {0}...", KeyFile);
-
-        assembly.Write(Assembly, MakeWriterParameters(Assembly, KeyFile));
-
-        return true;
-      }
-      catch (FileNotFoundException)
-      {
-        // no files - no fun
-        return false;
-      }
-      catch (Exception e)
-      {
-        Log.LogMessage(e.Message);
-        return false;
-      }
-    }
-
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Kind Of Magic, Assembly enchanter. Copyright (c) Lex Lavnikov");
-
-      if (args == null) return;
-
-      var resolver = new PreloadingAssemblyResolver(null);
-
-      foreach (var file in args)
-      {
-        if (file.StartsWith("$"))
+        private static void Main(string[] args)
         {
-          resolver.Load(file.Substring(1));
-          continue;
+            Console.WriteLine("Kind Of Magic, Assembly enchanter. Copyright (c) Lex Lavnikov");
+
+            if (args == null) return;
+
+            using (var resolver = new PreloadingAssemblyResolver(null))
+            {
+                foreach (var file in args)
+                {
+                    if (file.StartsWith("$"))
+                    {
+                        resolver.Load(file.Substring(1));
+                        continue;
+                    }
+
+                    var parameters = MakeReaderParameters(file, resolver);
+                    using (var assembly = AssemblyDefinition.ReadAssembly(file, parameters))
+                    {
+                        // Silverlight Assembly
+                        var corlib = (AssemblyNameReference)assembly.MainModule.TypeSystem.CoreLibrary;
+                        if (corlib.PublicKeyToken[0] == 124 && !resolver.NoGAC)
+                            resolver.AddSilverlightDirectories(corlib.Version.Major);
+
+                        using (var processor = new Processor(parameters.AssemblyResolver, assembly.MainModule))
+                        {
+                            switch (processor.Process())
+                            {
+                                case MagicResult.MagicApplied:
+                                case MagicResult.NoMagicNeeded:
+                                    assembly.Write(file, MakeWriterParameters(file, null));
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        var parameters = MakeReaderParameters(file, resolver);
-        var assembly = AssemblyDefinition.ReadAssembly(file, parameters);
-
-        // Silverlight Assembly
-        var corlib = (AssemblyNameReference)assembly.MainModule.TypeSystem.CoreLibrary;
-        if (corlib.PublicKeyToken[0] == 124 && !resolver.NoGAC)
-          resolver.AddSilverlightDirectories(corlib.Version.Major);
-
-        var processor = new Processor(parameters.AssemblyResolver, assembly.MainModule);
-        switch (processor.Process())
+        private static ReaderParameters MakeReaderParameters(string file, IAssemblyResolver resolver)
         {
-          case MagicResult.MagicApplied:
-          case MagicResult.NoMagicNeeded:
-            assembly.Write(file, MakeWriterParameters(file, null));
-            break;
+            var result = new ReaderParameters
+            {
+                ReadingMode = ReadingMode.Deferred,
+                ReadWrite = true,
+                AssemblyResolver = resolver
+            };
+
+            if (File.Exists(Path.ChangeExtension(file, ".pdb")))
+            {
+                result.ReadSymbols = true;
+                result.SymbolReaderProvider = new PdbReaderProvider();
+            }
+
+            return result;
         }
-      }
-    }
 
-    static ReaderParameters MakeReaderParameters(string file, IAssemblyResolver resolver)
-    {
-      var result = new ReaderParameters
-      {
-        ReadingMode = ReadingMode.Deferred,
-        AssemblyResolver = resolver
-      };
-
-      if (File.Exists(Path.ChangeExtension(file, ".pdb")))
-      {
-        result.ReadSymbols = true;
-        result.SymbolReaderProvider = new PdbReaderProvider();
-      }
-
-      return result;
-    }
-
-    static WriterParameters MakeWriterParameters(string file, string key = null)
-    {
-      if (File.Exists(Path.ChangeExtension(file, ".pdb")))
-        return new WriterParameters
+        private static WriterParameters MakeWriterParameters(string file, string key = null)
         {
-          WriteSymbols = true,
-          SymbolWriterProvider = new PdbWriterProvider(),
-          StrongNameKeyPair = LoadSnKey(key)
-        };
+            if (File.Exists(Path.ChangeExtension(file, ".pdb")))
+                return new WriterParameters
+                {
+                    WriteSymbols = true,
+                    SymbolWriterProvider = new PdbWriterProvider(),
+                    StrongNameKeyPair = LoadSnKey(key)
+                };
 
-      return new WriterParameters
-      {
-        WriteSymbols = false,
-        StrongNameKeyPair = LoadSnKey(key)
-      };
-    }
+            return new WriterParameters
+            {
+                WriteSymbols = false,
+                StrongNameKeyPair = LoadSnKey(key)
+            };
+        }
 
-    static StrongNameKeyPair LoadSnKey(string key)
-    {
-      return key != null ? new StrongNameKeyPair(File.ReadAllBytes(key)) : null;
+        private static StrongNameKeyPair LoadSnKey(string key)
+        {
+            return key != null ? new StrongNameKeyPair(File.ReadAllBytes(key)) : null;
+        }
     }
-  }
 }
